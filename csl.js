eed3148acdd6da6e18aabf8874035f0769dd38b1
Max F. Albrecht <1@178.is>
2013-07-21 15:19:30 +0200

    better module structure, method 'list' calls back with list

--- csl.js
+++ csl.js
@@ -2,48 +2,90 @@ var fs = require('fs-extra'),
     path = require('path'),
     async = require('async');
 
-module.exports = citationstyles = function citationstyles(callback) {
+module.exports = csl = {
   
-  var config = {
-    "source": "citationstyles-org",
+  "config": {
+    "mypath": module.id,
     "ext": ".csl",
-    "mypath": module.id
-  };
+    "source": {
+      "independent": "./citationstyles-org",
+      "dependent": "./citationstyles-org/dependent"
+    }
+  },
   
-  fs.readdir(config.source, function (err, files) {
+  "_init": function (callback) {
+  
+    var self = this;
     
-    // filter the list
-    async.map(
-      files, 
-      function filter(string, callback) {
+    // just do it once
+    if (self._list && !self._list.length) {
+      return callback(null, self._list);
+    }
+    else {
+      self._list = [];
+    }
+      
+    // "independent" only, for now
+    var source = path.resolve(path.join(self.config.mypath, '..'),  self.config.source.independent);
+  
+    // - get list of independent styles …
+    fs.readdir(source, function (err, styles) {
+    
+      // - catch error or empty result
+      if (err || !styles) { 
+        callback(err || new Error("No styles found!")); 
+      }
         
-        // if it is "foo.csl"
-        if (path.extname(string) === config.ext) {
-          
-          var res = {
-            "id": string.replace(config.ext, ''), // "foo"
-            "filename": path.join(config.mypath, string)
+      // - filter the non-csl files from `styles` list
+      // - make obj for each item
+      function readStyles(styles, callback) {
+        // body...
+      }
+    
+      var list = self._list;
+      styles.forEach(function (item) {
+      
+        // - if it is "foo.csl"
+        if (path.extname(item) === self.config.ext) {
+            
+          // TODO: read some props from xml…
+          var obj = {
+            "id": item.replace(self.config.ext, ''), // "foo"
+            "path": path.resolve(self.config.mypath, '..', self.config.source.independent, item)
           };
-          
-          // push to results
-          return callback(null, res);
-          
-        }
-        // otherwise don't push (remove from list)
-        else {
-          callback(null);
-        }
+
+          // - push to results
+          list.push(null, obj);
         
-      },
-      callback
-    );
-    
-  })
+        }
+      
+      });
+      
+      // - callback with resulting array of objs
+      callback(null, {
+        "independent": list
+      });
+            
+    });
   
-}
+  }
+  
+};
 
+csl.list = function list (callback) {
+  
+  var self = this;
+  
+  // one-time init
+  csl._init(function (err, res) {
+    
+    if (err) { callback(err); }
 
-// example usage
-// citationstyles(function (err, res) {
-//   console.log(err || res);
-// });
+    // - callback with resulting array of objs
+    callback(null, {
+      "independent": self._list
+    });
+  
+  });
+  
+};
