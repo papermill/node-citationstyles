310e14b0b78872d3ef1e62d53b01326e806dc6d0
Max F. Albrecht <1@178.is>
2013-07-21 15:49:17 +0200

    test.js: update for correct output, check for hash, too

--- test/test.js
+++ test/test.js
@@ -1,14 +1,29 @@
 var csl = require('../csl'),
-    assert = require('assert');
+    assert = require('assert'),
+    crypto = require('crypto');
 
 var config = {
-  "stylecount": 853
+  "stylecount": 1684,
+  "hash": "998d969063e04c25f13c6a6aede4c37119125672"
 };
 
-csl(function (err, styles) {
+// test csl.list()
+csl.list(function (err, styles) {
   
+  if (err) throw err;
+
   // console.log(styles);
+
   
-  assert.strictEqual(styles.length, config.stylecount, "Number of styles in repo");
+  // - check if number of styles in list matches
+  // console.log(styles.independent.length);
+  assert.strictEqual(styles.independent.length, config.stylecount, "Number of styles in list");
+  
+  // - check if their hash changed (protect package maintainer against missing upstream updates)
+  var shasum = crypto.createHash('sha1');
+  shasum.update(styles.independent.toString());
+  var hash = shasum.digest('hex');
+  // console.log(hash);
+  assert.strictEqual(hash, config.hash, "Hash of styles list");
   
 })
